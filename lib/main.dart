0a4c917f5d9ff63c9cfeace38ce33f8160e8d5f4
eyasuyid <eyasuyid@gmail.com>
2023-04-13 13:34:56 +0300

    Completed magic ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballnumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballnumber = Random().nextInt(4) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballnumber.png'),
+        ),
+      ),
+    );
+  }
+}
